c1de1e6ffae640dc33cc852be638f5c6d7c0af3d
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-07-06 00:28:41 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int arr[] = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            arr[ch - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (arr[ch - 'a'] != 0) {
+                arr[ch - 'a']--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
